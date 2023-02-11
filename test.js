b7d893bec439f46b90ad78772ada1cb39dc47974
Anay Pampatwar <pampatwar.a@northeastern.edu>
2023-02-11 13:49:33 -0500

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ const assert = chai.assert;
 describe('A simple arithmetic function', function () {
     it('should return the sum of two numbers', function () {
         const sum = (a, b) => a + b;
-        assert.equal(sum(2, 3), 5);
+        assert.equal(sum(2, 3), 4);
     });
 });
