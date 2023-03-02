78d957d657b1372725b1769b42c073100fd1164f
Anay-Pampatwar <pampatwar.a@northeastern.edu>
2023-03-02 03:44:41 -0500

    Assignment6Recreated

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -24,10 +24,10 @@ const auth = require('basic-auth');
 
 const s3 = new S3Client({
     region: process.env.S3_REGION,
-    credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+    // credentials: {
+    // accessKeyId: process.env.AWS_ACCESS_KEY,
+    // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    // }
 });
 
 
